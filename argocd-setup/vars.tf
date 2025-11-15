46e6d6f3ea919549dc5f8d21684a0fd61ae4e36a
DeXT <vladyslav.matsalkovskyi@gmail.com>
2025-11-15 14:47:49 +0200

    BULL-42: add all data to data.tf file

--- argocd-setup/vars.tf
+++ argocd-setup/vars.tf
@@ -12,21 +12,6 @@ variable "cluster-name" {
   description = "name of transferred EKS cluster"
 }
 
-data "aws_eks_cluster" "illuminati_eks" {
-  name = var.cluster-name
-}
-
-data "aws_subnets" "private_subnets" {
-  filter {
-    name   = "tag:Name"
-    values = ["private-us-east-1a-${var.env}", "private-us-east-1b-${var.env}"]
-  }
-}
-
-data "aws_eks_cluster_auth" "illuminati_eks" {
-  name = var.cluster-name
-}
-
 variable "argocd-chart-version" {
   description = "version of argoCD helm chart"
   type        = string
