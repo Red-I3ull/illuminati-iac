77924be94226dda0f8045c93696728340737b9ae
larysalozynska <llozy@softserveinc.com>
2025-11-21 10:58:54 +0000

    Setup AWS Load Balancer controller and Ingress

--- illuminaty-setup/modules/frontend-release/frontend-release.tf
+++ illuminaty-setup/modules/frontend-release/frontend-release.tf
@@ -14,19 +14,13 @@ resource "helm_release" "frontend" {
   namespace = kubernetes_namespace.app_namespace.metadata[0].name
   chart     = "${path.module}/../helm-charts/frontend"
 
-  values = [
-    templatefile("${path.module}/../helm-charts/frontend/values.yaml.tpl", {
-      frontend_image_repository = var.frontend_image_repository
-      frontend_image_tag        = var.frontend_image_tag
-      frontend_replicas         = var.frontend_replicas
-    })
-  ]
-
   depends_on = [
     kubernetes_namespace.app_namespace,
     var.illuminati-eks-nodes-id
   ]
-
+  values = [
+    file("./modules/helm-charts/frontend/values-stage-01.yaml")
+  ]
   atomic          = true
   cleanup_on_fail = true
   timeout         = 600
