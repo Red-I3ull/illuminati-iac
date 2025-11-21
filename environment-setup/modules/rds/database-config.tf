bfab3ff44ea31738a69ba4cb001e0aa3fcc98698
larysalozynska <llozy@softserveinc.com>
2025-11-21 11:08:21 +0000

    fix sonarqube

--- environment-setup/modules/rds/database-config.tf
+++ environment-setup/modules/rds/database-config.tf
@@ -64,8 +64,8 @@ resource "aws_db_instance" "mariadb" {
   region               = var.region
   availability_zone    = var.availability-zone
   db_subnet_group_name = aws_db_subnet_group.db_subnets.name
-  #  storage_encrypted       = true
-  #  backup_retention_period = 7
+  storage_encrypted       = true
+  backup_retention_period = 7
 
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
