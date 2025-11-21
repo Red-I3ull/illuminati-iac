01d4408bbddb59de85f68c682008b377a1117eea
DeXT <vladyslav.matsalkovskyi@gmail.com>
2025-11-21 15:34:54 +0200

    BULL-46: delete duplicated code

--- environment-setup/modules/eks/eks-config.tf
+++ environment-setup/modules/eks/eks-config.tf
@@ -49,14 +49,3 @@ resource "aws_eks_cluster" "illuminati-eks" {
 
   depends_on = [aws_iam_role_policy_attachment.illuminati-eks-AmazonEKSClusterPolicy]
 }
-
-
-data "tls_certificate" "illuminati" {
-  url = aws_eks_cluster.illuminati-eks.identity[0].oidc[0].issuer
-}
-
-resource "aws_iam_openid_connect_provider" "illuminati" {
-  url             = aws_eks_cluster.illuminati-eks.identity[0].oidc[0].issuer
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.illuminati.certificates[0].sha1_fingerprint]
-}
